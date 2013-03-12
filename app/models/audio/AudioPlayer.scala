64d7c6824695bff764800bd4be1f868b3e53a575
Travis Kaufman <travis.kaufman@gmail.com>
2013-03-12 19:26:51 -0400

    More verbose logging, add removePlayer and getPlayerVoice methods, tweak some method sigs for clients

--- app/models/audio/AudioPlayer.scala
+++ app/models/audio/AudioPlayer.scala
@@ -60,14 +60,65 @@ object AudioPlayer {
    * Adds a player to the group of players, giving him/her an instrument.
    *
    * @param playerId The unique Id of the player currently playing.
+   *
+   * @return The playerId that was added successfully, or null if a player
+   *    could not be added.
    */
-  def addPlayer(playerId: String) {
-    Logger.info("Adding player %s".format(playerId))
+  def addPlayer(playerId: String): String = {
+    Logger.info("Attempting to add player %s".format(playerId))
+
+    if (players.contains(playerId)) {
+      Logger.warn("Failed to add player %s: that Id already exists".format(
+        playerId
+      ))
+      return null
+    }
+
     val playerVoice = new WaveShapingVoice
     connectToOutput(playerVoice)
     synth.add(playerVoice)
     players += (playerId -> playerVoice)
     playerCount += 1
+
+    playerId
+  }
+
+  /**
+   * Removes a player from the group of players.
+   *
+   * @param playerId The unique Id of the player to be removed.
+   *
+   * @return The UnitVoice of the player that was removed if successful, null
+   *    otherwise.
+   */
+  def removePlayer(playerId: String): UnitVoice = {
+    Logger.info("Attempting to remove player %s".format(playerId))
+    val playerVoice = getPlayerVoice(playerId)
+
+    if (playerVoice != null) {
+      playerVoice.noteOff(synth.createTimeStamp)
+      players -= playerId
+      playerCount -= 1
+      Logger.info("Player %s successfully removed".format(playerId))
+    } else {
+      Logger.warn("Attempted to remove %s but playerId wasn't found".format(
+        playerId
+      ))
+    }
+
+    playerVoice
+  }
+
+  /**
+   * Get the voice associated with the playerId.
+   *
+   * @param playerId The player ID the voice is associated with.
+   *
+   * @return The voice associated with the playerId, or null if there is
+   *    no voice associated with that id.
+   */
+  def getPlayerVoice(playerId: String): UnitVoice = {
+    players.getOrElse(playerId, null)
   }
 
   /**
@@ -76,15 +127,23 @@ object AudioPlayer {
    * @param playerId The ID of the player whos voice should be played.
    */
   def play(playerId: String) {
-    val voice = players.getOrElse(playerId, null)
+    val voice = getPlayerVoice(playerId)
 
     if (voice == null) {
+      Logger.warn("cannot play voice of %s since no voice for that player " +
+                  "exists".format(playerId))
       return
     }
 
+    val amplitude = 1 / (if (playerCount > 0) playerCount else 1)
+    Logger.debug("(Theoretical) Amplitude value: %d".format(amplitude))
+
+
     // Play for 500 ms a tone of 250Hz at amplitude 0.4
     voice.noteOn(
-      250, 0.7, synth.createTimeStamp
+      250,
+      0.7,
+      synth.createTimeStamp
     )
     voice.noteOff(
       synth.createTimeStamp.makeRelative(0.5)
@@ -95,6 +154,14 @@ object AudioPlayer {
    * Shutdown the System.
    */
   def shutdown() {
+    val removed = players.keysIterator.map(removePlayer).filter { v =>
+      v match {
+        case null => false
+        case _ => true
+      }
+    }
+
+    Logger.debug("removed %d current players".format(removed.length))
     lineOut.stop()
     synth.stop()
   }
