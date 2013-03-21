b9666cee2445d6a17cbc80755bf0ee924ac30243
Travis Kaufman <travis.kaufman@gmail.com>
2013-03-20 21:13:47 -0400

    Start writing unit and integration tests. Put newline at eof

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,14 +1,16 @@
+/**
+ * @package test
+ */
 package test
 
 import org.specs2.mutable._
 
 import play.api.test._
 import play.api.test.Helpers._
+import play.api.libs.json.Json.parse
 
 /**
- * Add your spec here.
- * You can mock out a whole application including requests, plugins etc.
- * For more information, consult the wiki.
+ * Spec for the application controller. Should be pretty straightforward.
  */
 class ApplicationSpec extends Specification {
 
@@ -22,11 +24,28 @@ class ApplicationSpec extends Specification {
 
     "render the index page" in {
       running(FakeApplication()) {
-        val home = route(FakeRequest(GET, "/")).get
+        val home = route(FakeRequest()).get
 
         status(home) must equalTo(OK)
         contentType(home) must beSome.which(_ == "text/html")
-        contentAsString(home) must contain ("Your new application is ready.")
+        contentAsString(home) must contain ("JAMLive!")
+      }
+    }
+
+    "connect a user to the audio server if s/he has a unique ID" in {
+      val playerId = "Bob"
+
+      running(FakeApplication()) {
+        val connectRequest = route(FakeRequest("POST", "/connect").withFormUrlEncodedBody(
+          ("playerId" -> playerId)
+        )).get
+
+        status(connectRequest) must equalTo(OK)
+        contentType(connectRequest) must beSome.which(_ == "application/json")
+
+        val resp = parse(contentAsString(connectRequest))
+        (resp \ "status").as[Int] must equalTo(OK)
+        (resp \ "playerId").as[String] must equalTo(playerId)
       }
     }
   }
